26c17c3fc7f9d1f6304810cf82eed77fce37662d
milanmardia <72716536+milanmardia@users.noreply.github.com>
2021-02-26 14:53:10 -0600

    a

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PathfindingVisualizer extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            nodes: []
+        }
+    }
+}
+
+export default PathfindingVisualizer
